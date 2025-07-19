552656ff110da56b4120e330c97eda282cd505af
Gustavo Pedrosa <luizgfju@hotmail.com>
2025-07-18 22:23:18 -0300

    Initial commit

---
+++ Curso BackEnd 1/001 - Exercicio 1.py
@@ -0,0 +1,7 @@
+num1 = 1.024
+num2 = 2
+
+divisao = num1 / num2
+print("Resultado:", divisao)
+print("Eu estudo Python")
+
---
+++ Curso BackEnd 1/02 - Fundamentos de coleções.py
@@ -0,0 +1,10 @@
+
+# Lista manual
+lista = ['Maria', 23, 'Porto Alegre']
+print(lista[0])
+print(lista[1])
+print(lista[2])
+
+# Elementos sendo acessados individualmente
+n = lista.index('Porto Alegre')
+print(n)
